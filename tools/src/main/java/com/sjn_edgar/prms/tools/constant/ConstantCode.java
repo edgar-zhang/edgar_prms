d27a2d3c97814f3ecbd39a6510d804f59a59bcab
edgar-zhang <1129355377@qq.com>
2016-08-16 18:06:13 +0800

    添加后台支付系统--payhome.（添加shiro (1))

---
+++ tools/src/main/java/com/sjn_edgar/prms/tools/constant/ConstantCode.java
@@ -0,0 +1,14 @@
+package com.sjn_edgar.prms.tools.constant;/*
+* Copyright (c) 2016 www.51cjhb.com. All Rights Reserved.
+*/
+
+/**
+ * <p>Title:常量参数类 </p>
+ * <p>Description:设置全局常量参数 </p>
+ * @author edgar 【zbo@51qmz.cn】
+ * @version 1.0
+ * @create 2016 08 16 18:03
+ */
+public final class ConstantCode {
+}
+
